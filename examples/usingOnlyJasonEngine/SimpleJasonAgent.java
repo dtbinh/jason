42afa66bf6f37b39b9f8fae21ff874407e032285
jomifred <jomifred@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2005-08-10 20:13:54 +0000

    updated to 0.7

--- examples/usingOnlyJasonEngine/SimpleJasonAgent.java
+++ examples/usingOnlyJasonEngine/SimpleJasonAgent.java
@@ -3,12 +3,16 @@
 import jason.architecture.AgentArchitecture;
 import jason.asSemantics.ActionExec;
 import jason.asSemantics.Agent;
-import jason.asSemantics.Intention;
 import jason.asSemantics.TransitionSystem;
-import jason.asSyntax.BeliefBase;
 import jason.asSyntax.Literal;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 
 /**
  * Example of an agent that only uses Jason BDI engine.
@@ -17,55 +21,63 @@ import java.util.List;
  * The class must implement AgentArchitecture to be used by the Jason engine.
  */
 public class SimpleJasonAgent implements AgentArchitecture {
-    
-	public static void main(String[] a) {
-		SimpleJasonAgent ag = new SimpleJasonAgent();
-		ag.run();
-	}
 
-	// The BDI Engine (where the AS Semantics is implemented) 
-	protected TransitionSystem fTS = null;
+    private static Logger logger = Logger.getLogger(SimpleJasonAgent.class);
+    
+    public static void main(String[] a) {
+    	// Jason classes uses log4j to output messages,
+    	// so we need to configure it.
+       	Logger.getRootLogger().addAppender(new ConsoleAppender(new PatternLayout("[%c{1}] %m%n")));
+       	Logger.getRootLogger().setLevel(Level.INFO);
+
+       	SimpleJasonAgent ag = new SimpleJasonAgent();
+       	ag.run();
+    }
+
+    // The BDI Engine (where the AS Semantics is implemented) 
+    protected TransitionSystem fTS = null;
 	
     public SimpleJasonAgent() {
-        // set up the Jason agent
-        try {
-            Agent ag = new Agent();
+    	// set up the Jason agent
+		try {
+			Agent ag = new Agent();
 			// Create args for the agent
-			//   args[1] = AgentSpeak source
-			//   args[2] = Jason Directory 
-			String[] args = {null, "demo.asl", "../.."};
-            fTS = ag.initAg(args, this);
-	    } catch (Exception e) {
-            e.printStackTrace();
-        }
+			// args[1] = AgentSpeak source
+			String[] args = { null, "demo.asl" };
+			fTS = ag.initAg(args, this);
+		} catch (Exception e) {
+			logger.error("Init error", e);
+		}
     }
     
     public void run() {
 		try {
-			while (true) {
+			while (isRunning()) {
 				// calls the Jason engine to perform one reasoning cycle
+				logger.debug("Reasoning....");
 				fTS.reasoningCycle();
-	        }
+			}
 		} catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-	
-	public String getAgName() {
-		return "JasonAgent";
+			logger.error("Run error",e);
+		}
 	}
-
-	// this method just add some belief in the agent Belief Set
-	public void brf() {
-		Literal bel = Literal.parseLiteral("x(10)");
-        fTS.getAg().addBel(bel, BeliefBase.TSelf, fTS.getC(), Intention.EmptyInt);
+	
+    public String getAgName() {
+    	return "JasonAgent";
     }
 
-	// this method get the agent actions
-	public void act() {
+    // this method just add some perception for the agent
+    public List perceive() { 
+    	List l = new ArrayList();
+    	l.add(Literal.parseLiteral("x(10)"));
+    	return l;
+    }
+
+    // this method get the agent actions
+    public void act() {
     	ActionExec acExec = fTS.getC().getAction(); 
         if (acExec != null) {
-			System.out.println("Agent "+getAgName()+" is doing: "+acExec.getActionTerm());
+        	logger.info("Agent "+getAgName()+" is doing: "+acExec.getActionTerm());
         }
     }
     
@@ -74,11 +86,9 @@ public class SimpleJasonAgent implements AgentArchitecture {
     }
 	
     // Not used methods 
-	// These simple agent does not have messages/control/...
-	public List perceive() { return null; }
+    // This simple agent does not need messages/control/...
     public void sendMsg(jason.asSemantics.Message m) throws Exception {}
     public void broadcast(jason.asSemantics.Message m) throws Exception {}    
     public void checkMail() {}
-	public void informCycleFinished(boolean breakpoint) {}
-
+    public void informCycleFinished(boolean breakpoint) {}
 }
