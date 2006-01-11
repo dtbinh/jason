e50814353ad6673cb3fc39c3a9cd3a3bc4b2f02e
jomifred <jomifred@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2006-01-11 14:05:33 +0000

    no message

--- bin/jason.bat
+++ bin/jason.bat
@@ -1,5 +1,5 @@
 @echo off
 SET JAVA_HOME=C:\Program Files\jdk1.5
 SET PATH="%JAVA_HOME%\bin";%PATH%
-java -cp jason.jar;..\lib\log4j.jar;..\lib\saci.jar;..\lib\jedit\jedit.jar org.gjt.sp.jedit.jEdit
+java -cp jason.jar;..\lib\saci.jar;..\lib\jedit\jedit.jar org.gjt.sp.jedit.jEdit
 
