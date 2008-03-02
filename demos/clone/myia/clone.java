fec50c6c27b53db9682188e2bdc3d0e4bfe9a6f5
jomifred <jomifred@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2008-03-02 15:19:15 +0000

    add clone in the infra interface; improve "stop" system.

--- demos/clone/myia/clone.java
+++ demos/clone/myia/clone.java
@@ -2,15 +2,12 @@
 
 package myia;
 
-import jason.architecture.AgArch;
-import jason.asSemantics.Agent;
 import jason.asSemantics.DefaultInternalAction;
 import jason.asSemantics.TransitionSystem;
 import jason.asSemantics.Unifier;
 import jason.asSyntax.StringTerm;
 import jason.asSyntax.Term;
-import jason.infra.centralised.CentralisedAgArch;
-import jason.infra.centralised.RunCentralisedMAS;
+import jason.runtime.RuntimeServicesInfraTier;
 
 public class clone extends DefaultInternalAction {
 
@@ -18,26 +15,9 @@ public class clone extends DefaultInternalAction {
     public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
     	
     	String agName = ((StringTerm)args[0]).getString();
-    	
-    	// create infrastructure architecture (it works only for centralised infra!)
-    	CentralisedAgArch infraArch = new CentralisedAgArch();
-    	infraArch.setAgName(agName);
-    	infraArch.setEnvInfraTier(RunCentralisedMAS.getRunner().getEnvironmentInfraTier());
-    	RunCentralisedMAS.getRunner().addAg(infraArch);
-    	
-    	// create user level architecture
-    	AgArch arch = new AgArch();
-        arch.setArchInfraTier(infraArch);
-        
-        // create the agent clone
-        Agent clone = ts.getAg().clone(arch);
-        arch.setTS(clone.getTS());
-        
-        // start the agent
-        infraArch.setUserAgArch(arch);
-    	infraArch.setLogger();
-        (new Thread(infraArch, agName)).start();
-        
+    	RuntimeServicesInfraTier services = ts.getUserAgArch().getArchInfraTier().getRuntimeServices();
+        services.clone(ts.getAg(), ts.getUserAgArch().getClass().getName(), agName);
+
         return true;
     }
 }
