ed4992fd22f74fec617e25eea7089da2e87e79e0
jomifred <jomifred@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2008-06-24 13:15:06 +0000

    fix bug in =.. start fixing the bug reported by Sthepen

---
+++ applications/as-unit-test/src/jason/tests/BugVarsAsArg.java
@@ -0,0 +1,39 @@
+package jason.tests;
+
+import jason.asunit.TestAgent;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/** based on bug found by Stephen Cranefield  -- see jason list */
+public class BugVarsAsArg {
+
+    TestAgent ag; 
+
+    // initialisation of the agent test
+    @Before
+    public void setupAg() {
+        ag = new TestAgent();
+        
+        // defines the agent's AgentSpeak code
+        ag.parseAScode(
+                "test_rule(A,a(A)). "+ 
+                "ml0(L,L). "+
+                "ml(V1,V2,R) :- ml0([V1,V2],R). "+
+                "+!test1 <- ?test_rule(T,A); A = a(V); T=45; jason.asunit.print(V). "+
+                "+!test2 <- ?ml(A,B,L); A=1; B=2; jason.asunit.print(L). "
+        );
+    }
+    
+    @Test
+    public void testRule1() {
+    	ag.addGoal("test1");
+        ag.assertPrint("45", 5); 
+    }
+
+    @Test
+    public void testRule2() {
+    	ag.addGoal("test2");
+        ag.assertPrint("[1,2]", 5); 
+    }
+}
--- applications/as-unit-test/src/jason/tests/TestAll.java
+++ applications/as-unit-test/src/jason/tests/TestAll.java
@@ -8,6 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({
     BugListReturnUnification.class,
     BugVarsInInitBels.class,
+    BugVarsAsArg.class,
     TestAddLogExprInBB.class,
     TestGoalSource.class, 
     TestIF.class,
