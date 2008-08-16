be819878d8068484e71c4fd265c7f85b6443fd9c
jomifred <jomifred@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2008-08-16 10:13:14 +0000

    use the classical "deref" for vars binding instead of VarsCluster

--- applications/as-unit-test/src/jason/tests/BugVarsAsArg.java
+++ applications/as-unit-test/src/jason/tests/BugVarsAsArg.java
@@ -20,12 +20,19 @@ public class BugVarsAsArg {
                 "test_rule(A,a(A)). "+ 
                 "ml0(L,L). "+
                 "ml(V1,V2,R) :- ml0([V1,V2],R). "+
+                
+                "append([], L, L). "+
+                "append([H|T], L1, [H|L2]) :- append(T, L1, L2). "+
+
                 "+!test1 <- ?test_rule(T,A); A = a(V); T=45; jason.asunit.print(V). "+
-                "+!test2 <- ?ml(A,B,L); A=1; B=2; jason.asunit.print(L). "
+                "+!test2 <- ?ml(A,B,L); A=1; B=2; jason.asunit.print(L). "+
+                "+!test3 <- L=[X,Y]; ?append(L, [Z], L2); Z=a; X=f; Y=i; jason.asunit.print(L2). "
         );
     }
     
-    @Test
+    
+    
+    @Test 
     public void testRule1() {
     	ag.addGoal("test1");
         ag.assertPrint("45", 5); 
@@ -35,5 +42,11 @@ public class BugVarsAsArg {
     public void testRule2() {
     	ag.addGoal("test2");
         ag.assertPrint("[1,2]", 5); 
+    }
+
+    @Test
+    public void testRule3() {
+        ag.addGoal("test3");
+        ag.assertPrint("[f,i,a]", 10); 
     }
 }
