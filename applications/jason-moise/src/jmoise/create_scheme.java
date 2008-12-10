403f3bb3b4b39a9138fc84a0e60811fedab813c4
jomifred <jomifred@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2008-12-10 07:03:29 +0000

    fix problem in ConcurrentIA minor changes everywhere

--- applications/jason-moise/src/jmoise/create_scheme.java
+++ applications/jason-moise/src/jmoise/create_scheme.java
@@ -6,7 +6,7 @@ import jason.asSyntax.Term;
 
 /** 
 
-<p>Organisational action: <b><code>jmoise.create_scheme( [sch_name(SchId), ] SchSpecId [, ListOfRespGrs] )</code></b>: 
+<p>Organisational action: <b><code>jmoise.create_scheme( [sch_name(SchId), ] SchSpecId [, ListOfRespGrs] [, newSchId] )</code></b>: 
    used by an agent to create a new scheme. 
    The first optional parameter is the name of the new scheme.
    The third optional parameter is a list of initial responsible groups.
@@ -30,7 +30,7 @@ import jason.asSyntax.Term;
      groups.</li>
 <li> <code>jmoise.create_scheme(wp, [g1, g2], S)</code>: 
      creates new scheme based on specification wp and set g1 and g2 as responsible
-     groups. The identification of the new group will be unified with G.</li>
+     groups. The identification of the new scheme will be unified with S.</li>
 </ul>
  
 @see jmoise.remove_scheme 
