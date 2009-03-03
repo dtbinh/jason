d92eafccc8205169beb45591d0832f4cce4bd473
jomifred <jomifred@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2009-03-03 07:42:18 +0000

    fix doc of term2string, improve foreach loop

--- applications/as-unit-test/src/jason/tests/BugVarsAsArg.java
+++ applications/as-unit-test/src/jason/tests/BugVarsAsArg.java
@@ -5,7 +5,7 @@ import jason.asunit.TestAgent;
 import org.junit.Before;
 import org.junit.Test;
 
-/** based on bug found by J¿rgen Villadsen */
+/** based on bug found by Stephen Cranefield  -- see jason list */
 public class BugVarsAsArg {
 
     TestAgent ag; 
@@ -17,17 +17,35 @@ public class BugVarsAsArg {
         
         // defines the agent's AgentSpeak code
         ag.parseAScode(
-                "+!g <- +p(ggg); !gg. " + 
-                "+!gg : p(X) <- !!X. " +
-                "+!ggg[source(A)] <- jason.asunit.print(A). "
+                "test_rule(A,a(A)). "+ 
+                "ml0(L,L). "+
+                "ml(V1,V2,R) :- ml0([V1,V2],R). "+
+                
+                "append([], L, L). "+
+                "append([H|T], L1, [H|L2]) :- append(T, L1, L2). "+
+
+                "+!test1 <- ?test_rule(T,A); A = a(V); T=45; jason.asunit.print(V). "+
+                "+!test2 <- ?ml(A,B,L); A=1; B=2; jason.asunit.print(L). "+
+                "+!test3 <- L=[X,Y]; ?append(L, [Z], L2); Z=a; X=f; Y=i; jason.asunit.print(L2). "
         );
     }
     
-    
-    
+        
     @Test 
-    public void testGoal() {
-    	ag.addGoal("g");
-        ag.assertPrint("self", 10); 
+    public void testRule1() {
+        ag.addGoal("test1");
+        ag.assertPrint("45", 5); 
+    }
+
+    @Test
+    public void testRule2() {
+        ag.addGoal("test2");
+        ag.assertPrint("[1,2]", 5); 
+    }
+
+    @Test
+    public void testRule3() {
+        ag.addGoal("test3");
+        ag.assertPrint("[f,i,a]", 10); 
     }
 }
