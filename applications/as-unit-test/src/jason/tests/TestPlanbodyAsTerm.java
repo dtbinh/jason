475ee6c6f03e6af753925f7ab73ef47046c31d62
jomifred <jomifred@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2009-03-17 17:00:23 +0000

    improve tests for vars as plan body

--- applications/as-unit-test/src/jason/tests/TestPlanbodyAsTerm.java
+++ applications/as-unit-test/src/jason/tests/TestPlanbodyAsTerm.java
@@ -21,8 +21,16 @@ public class TestPlanbodyAsTerm {
                 "+!test3 <- !g2(-1 + 2)."+
                 "+!test4 <- X = {a(1); b; c}; !g(X)."+
                 
+                "+!test5 <- !g5(R); jason.asunit.print(R). "+
+                "+!g5(X) <- C; .print(errrrrror). "+
+                "-!g5(X)[error(I),error_msg(M)] <- jason.asunit.print(I,M); X = ok. "+
+
+                "+!test6 <- A = { a }; B = { A }; C = { B }; C; jason.asunit.print(end). "+
+                
+                "+!test7(X) <- A = { jason.asunit.print(a,X); jason.asunit.print(b,X*2) }; A; jason.asunit.print(end,X/2). "+
+                
                 "+!g({A; R}) <- A; !g(R). "+
-                "+!g(A)      <- A." +
+                "+!g(A)    <- A." +
                 "+!g2(A)     <- jason.asunit.print(A)."
         );
     }
@@ -31,7 +39,7 @@ public class TestPlanbodyAsTerm {
     public void testProgram1a() {
         ag.addGoal("start");
         ag.assertBel("g({a(1);b;c})", 5);
-        ag.assertAct("a(1)", 4);
+        ag.assertAct("a(1)", 5);
         ag.assertAct("b", 4);
         ag.assertAct("c", 4);
     }
@@ -56,4 +64,25 @@ public class TestPlanbodyAsTerm {
         ag.assertPrint("1", 5);
     }
 
+    @Test
+    public void test5() {
+        ag.addGoal("test5");
+        ag.assertPrint("body_var_ungroundstringcode:1: Variable 'C' must be ground.",10);
+        ag.assertPrint("ok", 10);
+    }
+
+    @Test
+    public void test6() {
+        ag.addGoal("test6");
+        ag.assertAct("a", 5);
+        ag.assertPrint("end", 5);
+    }
+
+    @Test
+    public void test7() {
+        ag.addGoal("test7(100)");
+        ag.assertPrint("a100", 5);
+        ag.assertPrint("b200", 5);
+        ag.assertPrint("end50", 5);
+    }
 }
