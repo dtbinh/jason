73599c6b1d6e3f65653af02d1e6dece81b74c2e2
jomifred <jomifred@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2009-08-17 20:31:21 +0000

    fix bug in succeed_goal

--- applications/as-unit-test/src/jason/tests/TestAll.java
+++ applications/as-unit-test/src/jason/tests/TestAll.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
     BugListReturnUnification.class,
+    BugSucceedGoal.class,
     BugVarsAsArg.class,
     BugVarsAsGoal.class, 
     BugVarsInInitBels.class,
