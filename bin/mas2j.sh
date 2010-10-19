ab50a9064b07dfadbbbe0e5985b9e144f796508b
jomifred <jomifred@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2010-10-19 12:17:25 +0000

    allows the definition of a class that allocates agents to hosts when using JADE

--- bin/mas2j.sh
+++ bin/mas2j.sh
@@ -19,7 +19,7 @@ JASONDIR=`pwd`
 
 cd $CURDIR
 
-java -classpath $JASONDIR/lib/jason.jar:$JASONDIR/lib/saci.jar:$JASONDIR/lib/jade.jar:. \
+java -classpath bin/classes:$JASONDIR/lib/jason.jar:$JASONDIR/lib/saci.jar:$JASONDIR/lib/jade.jar:. \
   jason.mas2j.parser.mas2j $1 $2
 
 #chmod u+x *.sh
