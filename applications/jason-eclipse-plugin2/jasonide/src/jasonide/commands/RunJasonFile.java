f7f6c225599f033bfe75a20abfc4db77c9758d1f
xsplyter <xsplyter@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2012-04-11 22:47:10 +0000

    Added the new version of Jason eclipse plugin.

---
+++ applications/jason-eclipse-plugin2/jasonide/src/jasonide/commands/RunJasonFile.java
@@ -0,0 +1,91 @@
+package jasonide.commands;
+
+import jason.jeditplugin.MASLauncherInfraTier;
+import jason.mas2j.MAS2JProject;
+import jason.mas2j.parser.ParseException;
+import jason.mas2j.parser.TokenMgrError;
+import jasonide.core.Utils;
+
+import java.io.File;
+import java.io.StringReader;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class RunJasonFile implements IObjectActionDelegate {
+	private IFile jasonFile = null; 
+	
+	@Override
+	public void run(IAction action) {
+		runProject(false);
+	}
+	
+    private void runProject(final boolean debug) {
+    	System.out.println("Launching " + jasonFile.getProject().getName());
+        
+        new Thread() {
+            public void run() {
+            	MAS2JProject project = parseProject();
+                // launch the MAS
+                try {
+                	MASLauncherInfraTier masLauncher;
+                    masLauncher = project.getInfrastructureFactory().createMASLauncher();
+                    masLauncher.setProject(project);
+                    //masLauncher.setListener(arg0)
+                    if (masLauncher.writeScripts(debug)) {
+                        new Thread(masLauncher, "MAS-Launcher").start();
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }.start();
+    }
+    
+    private MAS2JProject parseProject() {
+        try {
+        	System.out.println("Parsing project file... ");
+
+        	//String text = loadFile(mainFile.getLocation().toString());
+        	String text = Utils.loadFile(jasonFile.getLocation().toString());
+        	
+            jason.mas2j.parser.mas2j parser = new jason.mas2j.parser.mas2j(new StringReader(text));
+            MAS2JProject project = parser.mas();
+            project.setDirectory(jasonFile.getProject().getLocation().toString());
+            project.setProjectFile(new File(jasonFile.getLocation().toString()));
+            project.fixAgentsSrc(null);
+            System.out.println(" parsed successfully!\n");
+            return project;
+        } catch (ParseException ex) {  
+        	System.out.println("\nmas2j: syntactic errors found... \n" + ex + "\n");
+        } catch (TokenMgrError ex) {
+        	System.out.println("\nmas2j: lexical errors found... \n" + ex + "\n");
+        } catch (Exception ex) {
+        	System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+        if (selection != null && selection instanceof ISelection) {
+            IStructuredSelection strucSel = (IStructuredSelection) selection;
+            
+            jasonFile = (IFile)strucSel.getFirstElement();
+        }
+		
+	}
+
+	@Override
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+
+	}
+}
