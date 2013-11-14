1563469813294131001c708faeb9bc228320ab1c
jomifred <jomifred@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2013-11-14 10:44:23 +0000

    add test, update moise

--- applications/as-unit-test/src/jason/tests/TestAll.java
+++ applications/as-unit-test/src/jason/tests/TestAll.java
@@ -14,6 +14,7 @@ import org.junit.runners.Suite.SuiteClasses;
     BugIfGoalReturn.class,
     BugIfLength.class,
     BugUnamedVars.class,
+    BugList1.class,
     TestAddLogExprInBB.class,
     TestGoalSource.class,
     TestIA.class, 
